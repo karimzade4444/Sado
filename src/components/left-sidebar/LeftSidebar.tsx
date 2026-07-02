4b1d799c6a9331b044e8aaf8e23d9c9bdc3fca99
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 20:17:55 +0500

    left

--- src/components/left-sidebar/LeftSidebar.tsx
+++ src/components/left-sidebar/LeftSidebar.tsx
@@ -3,14 +3,14 @@ import { Menu } from "./Menu";
 
 export function LeftSidebar() {
   return (
-    <aside>
+    <aside className=" border-r border-player-bg h-full">
       <Menu items={MENU_ITEMS} />
 
-      <hr className=" my-10 border-white/20 " />
+      <hr className=" my-10 border-player-bg " />
 
       <Menu items={LIBRARY_MENU_ITEMS} title="Library" />
 
-      <hr className=" my-10 border-white/20 " />
+      <hr className=" my-10 border-player-bg " />
 
       {/* <Menu items={MENU_ITEMS} title="Playlists" /> */}
     </aside>
