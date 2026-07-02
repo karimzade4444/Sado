37895c1b74adf32823bbcdbd121345cb9cce2184
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-07-02 16:29:47 +0500

    store

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import observerPlugin from "mobx-react-observer/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    observerPlugin(
+      // optional
+      { exclude: ["src/ui-components/**"] },
+    ),
+  ],
 });
